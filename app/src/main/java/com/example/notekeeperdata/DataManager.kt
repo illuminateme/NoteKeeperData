9bdb09f647403a676e734b0fd23960e51390b117
illuminateme <aimaatigari@gmail.com>
2021-09-09 19:55:55 +0100

    Data Manager Created

--- app/src/main/java/com/example/notekeeperdata/DataManager.kt
+++ app/src/main/java/com/example/notekeeperdata/DataManager.kt
@@ -1,6 +1,25 @@
 package com.example.notekeeperdata
 
-class DataManager
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
 
-{
-}g
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses() {
+        var course = CourseInfo("android_intents", "Android Programing with Intents")
+        courses.set(course.courseId, course)
+
+        course =
+            CourseInfo(courseId = "android_async", title = "Android Async Programming Services")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo(courseId = "java_core", title = "Java Fundamentals: The Core Platform")
+        courses.set(course.courseId, course)
+    }
+}
